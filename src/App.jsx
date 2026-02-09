921c8d3f38926a0d62add7efe1100220fb23a5be
menahasrinivas <menahasrinivas@gmail.com>
2026-02-09 15:38:52 +0530

    error path

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,7 @@ import Contact from "./pages/Contact";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import SafetyAudit from "./pages/energyAudit/safetyaudit";
-import EcmWcmActivities from "./pages/energyAudit/EcmWcmActivities";
-import WaterConservationAudit from "./pages/energyAudit/WaterConservationAudit";
-import ThermographyAudit from "./pages/energyAudit/ThermographyAudit";
+
 
 export default function App() {
   useEffect(() => {
